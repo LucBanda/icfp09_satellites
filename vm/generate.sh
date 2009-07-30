8196341ac0b5c3928d317199c5534314a9c86598
Luc Banda <Lucbanda@gmail.com>
2009-07-30 23:43:27 +0200

    replacing Makefile with CMake

--- vm/generate.sh
+++ vm/generate.sh
@@ -1,8 +1,10 @@
-make clean
-make generate=1
+
+cmake -DGENERATE=ON
+make
 ./vm ../scenarios 1001 > bin1.cpp
 ./vm ../scenarios 2001 > bin2.cpp
 ./vm ../scenarios 3001 > bin3.cpp
 ./vm ../scenarios 4001 > bin4.cpp
+cmake -DGENERATE=OFF
 make clean
 make
